4d17ade1a8f969c9806f64a698710188a24211e9
YunSwain <destap@naver.com>
2025-02-24 20:08:14 +0900

    [Feat]: First commit

---
+++ Conduit/ConduitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConduitApp.swift
+//  Conduit
+//
+//  Created by Swain Yun on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ConduitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConduitTests/ConduitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConduitTests.swift
+//  ConduitTests
+//
+//  Created by Swain Yun on 2/24/25.
+//
+
+import Testing
+@testable import Conduit
+
+struct ConduitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
